04fe94b233acba34dc59ce31748048813cee5e64
m1rojke <mirojkeeeee@gmail.com>
2023-06-23 16:27:06 +0300

    first

---
+++ ready-html/js/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+function binaryArrayToNumber(arr) {
+  console.log(arr)
+}
+binaryArrayToNumber([0,0,0,1])
